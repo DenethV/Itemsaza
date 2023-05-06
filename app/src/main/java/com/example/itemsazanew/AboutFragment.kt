7f51be52f9fd75a50b50617198ce3f79b5d4a101
Deneth <it21200342@my.sliit.lk>
2023-05-06 21:27:22 +0530

    Navigation

---
+++ app/src/main/java/com/example/itemsazanew/AboutFragment.kt
@@ -0,0 +1,11 @@
+package com.example.itemsazanew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_about)
+    }
+}
